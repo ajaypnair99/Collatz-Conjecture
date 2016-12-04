c19596b5a30d7413e43b7d5a88d86deb41a8ce47
ajaypnair99 <ajaypnair99@gmail.com>
2016-12-05 00:03:34 +0530

    Adding all files

---
+++ CollatzConjucture/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CollatzConjucture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputNumberBox = new System.Windows.Forms.TextBox();
+            this.ComputeButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.BenchmarkButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputNumberBox
+            // 
+            this.InputNumberBox.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputNumberBox.Location = new System.Drawing.Point(13, 13);
+            this.InputNumberBox.Multiline = true;
+            this.InputNumberBox.Name = "InputNumberBox";
+            this.InputNumberBox.Size = new System.Drawing.Size(267, 67);
+            this.InputNumberBox.TabIndex = 0;
+            // 
+            // ComputeButton
+            // 
+            this.ComputeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputeButton.Location = new System.Drawing.Point(286, 13);
+            this.ComputeButton.Name = "ComputeButton";
+            this.ComputeButton.Size = new System.Drawing.Size(160, 67);
+            this.ComputeButton.TabIndex = 1;
+            this.ComputeButton.Text = "Compute";
+            this.ComputeButton.UseVisualStyleBackColor = true;
+            this.ComputeButton.Click += new System.EventHandler(this.ComputeButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ResultLabel.Font = new System.Drawing.Font("Perpetua Titling MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(12, 171);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(434, 377);
+            this.ResultLabel.TabIndex = 2;
+            // 
+            // BenchmarkButton
+            // 
+            this.BenchmarkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BenchmarkButton.Location = new System.Drawing.Point(120, 86);
+            this.BenchmarkButton.Name = "BenchmarkButton";
+            this.BenchmarkButton.Size = new System.Drawing.Size(160, 67);
+            this.BenchmarkButton.TabIndex = 3;
+            this.BenchmarkButton.Text = "Benchmark";
+            this.BenchmarkButton.UseVisualStyleBackColor = true;
+            this.BenchmarkButton.Click += new System.EventHandler(this.BenchmarkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 557);
+            this.Controls.Add(this.BenchmarkButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.ComputeButton);
+            this.Controls.Add(this.InputNumberBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputNumberBox;
+        private System.Windows.Forms.Button ComputeButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button BenchmarkButton;
+    }
+}
+
---
+++ CollatzConjucture/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace CollatzConjucture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        BigInteger input,result; // Input takes input , and result is used for calculations
+
+
+        //Compute Button
+        private void ComputeButton_Click(object sender, EventArgs e)
+        {
+            input = BigInteger.Parse(InputNumberBox.Text);// converting the input from the textbox to biginteger
+
+            //converting input to a postive intger 
+            if (input < 0)
+            {
+                input = ~input + 1; // converting value of input to positive if the integer is negative
+                
+            }
+            
+            CollatzConjectureCalc(input); // calling the function and passing input as a value
+        }
+
+        //Benchmark Button
+        private void BenchmarkButton_Click(object sender, EventArgs e)
+        {
+            CollatzConjectureBenchmark(); // calling benchmark function
+
+        }
+
+       
+        //Collatz Conjecture checking function
+        int CollatzConjectureCalc(BigInteger input)
+        {
+            result = input;// setting the value of result as input
+
+            int steps =0; // initializing steps to 0
+
+           // var watch = Stopwatch.StartNew();//initializing stop watch
+
+            ResultLabel.Text = ""; // clearing the log label
+
+
+            while(result!=1 )
+            {
+                //Checking if even 
+                if (result % 2 == 0)
+                {
+                    result = result / 2;//dividing the result by 2
+
+                } else if (result%2 != 0) // if not even
+                {
+                    result = (3 * result) + 1; // Collatz Conjecture , equation
+                }
+                
+                steps++; // incrementing the steps
+            }
+           // watch.Stop(); // stopping the watch
+
+            //var elapsedtime = watch.ElapsedMilliseconds; // setting elapsed time to the time taken to execute the steps
+
+            ResultLabel.Text += "End Result = "+result; // displaying the result
+            //ResultLabel.Text += "\nElapsed Time =" + elapsedtime; // displaying time elapsed
+            ResultLabel.Text += "\nSteps taken = " + steps; // displaying the steps
+
+            return 0;
+        }
+
+        //Benchmarking function
+        private void CollatzConjectureBenchmark()
+        {
+            BigInteger counter = 10,steps=0; // setting values of counter and steps
+
+            BigInteger result, input=5;   
+            var watch = Stopwatch.StartNew();
+            for (counter = 1; counter <= 10000000; counter++) //change the counter before comitting
+            {
+                result = input;
+                while (result != 1) // loop will run till the result equals 1, after that it will loop as 4 , 2 ,1 so its of no use after 1
+                {
+                    if (result % 2 == 0)
+                    {
+                        result = result / 2;//dividing the result by 2
+
+                    }
+                    else if (result % 2 != 0) // if not even
+                    {
+                        result = (3 * result) + 1; // Collatz Conjecture , equation
+                    }
+                    
+                    steps++; // incrementing steps
+                }
+                input++; // incrementing input for next number in result
+            }
+            watch.Stop();//stopping the watch
+            var elapsedtime = watch.ElapsedMilliseconds; // setting the value of elapsed time 
+
+            ResultLabel.Text += "\nElapsed time : " + elapsedtime;//displaying elapsed time
+            ResultLabel.Text += "\nSteps taken : " + steps; //displaying steps taken
+        }
+    }
+}
